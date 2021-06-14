884fd7c7179241612d7636aea013ce03911e3fcc
unknown <sagetbob349@gmail.com>
2021-06-13 19:01:52 -0500

    first commit

---
+++ Models/AppointmentModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentMaker.Models
+{
+    public class AppointmentModel
+    {
+        [Required]
+        [StringLength(20,MinimumLength =4)]
+        [DisplayName("Patient's full name")]
+        public string PatientName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Appointment Date")]
+        public DateTime AppointmentDate { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayName("Patient's Net Worth")]
+        public decimal PatientNetWorth { get; set; }
+        [DisplayName("Name of the Doctor treating you")]
+        public string DoctorName { get; set; }
+        [Range(1,10)]
+        [DisplayName("Your pain level")]
+        public int PainLevel { get; set; }
+    }
+}
